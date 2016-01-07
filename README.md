27d6d3532ef97edc52edb93a774bf7de65a6445a
Simon Boudrias <admin@simonboudrias.com>
2016-01-06 21:22:15 -0500

    Extract code from yeoman-generator

---
+++ README.md
@@ -0,0 +1,29 @@
+# fetch-remote [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Fetch a remote zip and extract it in a cache folder
+
+## Installation
+
+```sh
+$ npm install --save fetch-remote
+```
+
+## Usage
+
+```js
+var fetchRemote = require('fetch-remote');
+
+fetchRemote('Rainbow');
+```
+## License
+
+ © [The Yeoman Team](http://yeoman.io)
+
+
+[npm-image]: https://badge.fury.io/js/fetch-remote.svg
+[npm-url]: https://npmjs.org/package/fetch-remote
+[travis-image]: https://travis-ci.org//fetch-remote.svg?branch=master
+[travis-url]: https://travis-ci.org//fetch-remote
+[daviddm-image]: https://david-dm.org//fetch-remote.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org//fetch-remote
+[coveralls-image]: https://coveralls.io/repos//fetch-remote/badge.svg
+[coveralls-url]: https://coveralls.io/r//fetch-remote
---
+++ test/remote.js
@@ -0,0 +1,78 @@
+/*global describe, it, before, beforeEach */
+'use strict';
+var os = require('os');
+var path = require('path');
+var fs = require('fs');
+var assert = require('yeoman-assert');
+var nock = require('nock');
+var tmpdir = path.join(os.tmpdir(), 'yeoman-remote');
+var remote = require('..');
+
+
+describe('remote()', function () {
+  beforeEach(function () {
+    nock('https://github.com')
+      .get('/yeoman/generator/archive/master.tar.gz')
+      .replyWithFile(200, path.join(__dirname, 'fixtures/testRemoteFile.tar.gz'));
+  });
+
+  it('remotely fetch a package on github', function (done) {
+    remote('yeoman', 'generator', done);
+  });
+
+  it('cache the result internally into a `_cache` folder', function (done) {
+    remote('yeoman', 'generator', function () {
+      fs.stat(path.join(cacheRoot(), 'yeoman/generator/master'), done);
+    }.bind(this));
+  });
+
+  it('invoke `cb` with a cachePath', function (done) {
+    remote('yeoman', 'generator', function (err, cachePath) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      assert.equal(cachePath, '');
+      done();
+    });
+  });
+
+});
+
+describe('remote.extract()', function () {
+  it('download and untar via the NPM download package', function (done) {
+    var scope = nock('http://example.com')
+      .get('/f.tar.gz')
+      .replyWithFile(200, path.join(__dirname, 'fixtures/testFile.tar.gz'));
+
+    remote.extract('http://example.com/f.tar.gz', tmp, function (err) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      assert.noFile(path.join(tmp, 'f.tar.gz'));
+      assert(scope.isDone());
+      done();
+    });
+  });
+});
+
+describe('remote.fetch()', function () {
+  it('allow the fetching of a single file', function (done) {
+    var scope = nock('http://example.com')
+      .get('/f.txt')
+      .replyWithFile(200, path.join(__dirname, 'fixtures/help.txt'));
+
+    remote.fetch('http://example.com/f.txt', tmp, function (err) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      assert(scope.isDone());
+      fs.stat(path.join(tmp, 'f.txt'), done);
+    });
+  });
+});
